c2ce364d052ca3eb1d3447b755d6128fcf6c809a
CosmicZan <omar_paramo@cosmic.com.mx>
2022-06-29 16:04:34 -0500

    Added Function

--- Python_Tests/Excel.py
+++ Python_Tests/Excel.py
@@ -1,38 +1,39 @@
 import xlsxwriter
 import decimal
 
-#Abre un archivo txt, r = Read
-file = open('src/Prueba.txt', 'r')
-f = file.readlines()
+def TxtExl():
+  #Abre un archivo txt, r = Read
+  file = open('src/hsbc.txt', 'r')
+  f = file.readlines()
 
-#Creación del Excel
-workbook = xlsxwriter.Workbook('src/Prueba.xls')
-worksheet = workbook.add_worksheet("Hoja1")
+  #Creación del Excel
+  workbook = xlsxwriter.Workbook('src/Prueba.xls')
+  worksheet = workbook.add_worksheet("Hoja1")
 
-#Creación de los Headers
-worksheet.write(0, 0, "Id")
-worksheet.write(0, 1, "Nombre")
-worksheet.write(0, 2, "RFC")
+  #Creación de los Headers
+  worksheet.write(0, 0, "Campo1")
+  worksheet.write(0, 1, "Campo2")
+  worksheet.write(0, 2, "Campo3")
 
-#Guarda contenido del Txt en un List
-newList = []
-for line in f:
- if line[-1] == '\n':
-   newList.append(line)
- else:
-   newList.append(line)
+  #Guarda contenido del Txt en un List
+  newList = []
+  for line in f:
+    if line[-1] == '\n':
+      newList.append(line)
+    else:
+      newList.append(line)
 
-row = 1
-column = 0
-count = 1 
-#Iteración en la newList e Inserción a Excel
-for item in newList:
-  worksheet.write(row, column, item)
-  column += 1
-  espacio = decimal.Decimal(column)/decimal.Decimal(3)
-  if(espacio == count):
-    row += 1
-    column = 0
-    count += 1
+  row = 1
+  column = 0
+  count = 1 
+  #Iteración en la newList e Inserción a Excel
+  for item in newList:
+    worksheet.write(row, column, item)
+    column += 1
+    espacio = decimal.Decimal(column)/decimal.Decimal(3)
+    if(espacio == count):
+      row += 1
+      column = 0
+      count += 1
 
-workbook.close()
+  workbook.close()
--- Python_Tests/Python_Tests.py
+++ Python_Tests/Python_Tests.py
@@ -1,19 +1,3 @@
-from tkinter import N
+from Excel import TxtExl
 
-
-#Prueba para Read de un Txt y pasarlo a un Xls
-
-#Abre un archivo txt, r = Read
-file = open('src/Prueba.txt', 'r')
-f = file.readlines()
-
-#Guarda contenido del Txt en un List
-newList = []
-for line in f:
-  if line[-1] == '\n':
-    newList.append(line)
-  else:
-    newList.append(line)
-
-#Imprime List
-print(newList)
+TxtExl()
