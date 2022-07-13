5166232e938643b9232826a6ff882ce85d6e551e
CosmicZan <omar_paramo@cosmic.com.mx>
2022-07-13 17:36:07 -0500

    Added Date

--- Python_Tests/CashManagement.py
+++ Python_Tests/CashManagement.py
@@ -1,9 +1,9 @@
 from openpyxl import load_workbook
 import decimal
+import datetime
 
 def TxtExl():
     TxtExl_Hoja1()
-    TxtExl_Hoja2()
 
 def TxtExl_Hoja1():
 
@@ -11,10 +11,19 @@ def TxtExl_Hoja1():
   file = open('src/hsbc.txt', 'r')
   f = file.readlines()
 
-  # Abre un Excel YA creado
-  file_path = 'src/Prueba.xlsx'
-  wb = load_workbook(file_path)
-  ws = wb['Hoja1']  # or wb.active
+  # Abre un Excel YA creado.
+  file_path = 'src/CashManagementBankStatementImportTemplate.xlsm'
+  wb = load_workbook(file_path, read_only=False, keep_vba=True)
+  ws = wb['Statement Headers']  # or wb.active
+
+  #Insertar Fecha a Excel 
+  item = Mes_Dia_Final(datetime.date(2022, 7, 1))
+  row = 4
+  col = 2
+  while ws.cell(row, col).value:
+    row+=1
+  ws["D" + "5"] = item
+  col += 1
 
   #Guarda contenido del Txt en un List
 
@@ -35,7 +44,7 @@ def TxtExl_Hoja1():
     #"Switch"
     #Caso Tag 20
     if(item.startswith(":20:")):
-      row = 1
+      row = 4
       col = 1
       colId = 0
       tag = item
@@ -50,13 +59,14 @@ def TxtExl_Hoja1():
           col += 1
     #Caso Tag 25
     elif(item.startswith(":25:")):
-      row = 1
-      col = 3
+      item = Mes_Dia_Final(datetime.date(2022, 7, 1))
+      row = 4
+      col = 2
       tag = item.split(":25:", 1)
       for items in enumerate(tag):
           while ws.cell(row, col).value:
             row+=1
-          ws["C" + str(row)] = items[1]
+          ws["B" + str(row)] = items[1]
           col += 1
     #Caso Tag 28C
     elif(item.startswith(":28C:")):
@@ -73,7 +83,7 @@ def TxtExl_Hoja1():
           col += 1
     #Caso Tag 60F
     elif(item.startswith(":60F:")):
-      row = 1
+      row = 4
       col = 6
       colId = 5
       tag = item
@@ -86,79 +96,7 @@ def TxtExl_Hoja1():
          colId += 1
          col += 1
     #Caso Tag 61 // Arreglar el salto de Línea
-    #elif(item.startswith(":61:")):
-    #  row = 1
-    #  col = 11
-    #  colId = 10
-    #  tag = item
-    #  index = [4, 10, 14, 15, 30, 34, 70]
-    #  parts = [tag[i:j] for i,j in zip(index, index[1:] + [None])]
-    #  for id, items in enumerate(parts):
-    #     while ws.cell(row, col).value:
-    #       row+=1
-    #     ws[column[colId] + str(row)] = items
-    #     colId += 1
-    #     col += 1
-    ##Caso Tag 86
-    #elif(item.startswith(":86:")):
-    #  row = 1
-    #  col = 17
-    #  tag = item.split(":86:", 1)
-    #  for items in enumerate(tag):
-    #      while ws.cell(row, col).value:
-    #        row+=1
-    #      ws["Q" + str(row)] = items[1]
-    #      col += 1
-    ##Caso Tag 62M
-    #elif(item.startswith(":62M:")):
-    #  row = 1
-    #  col = 18
-    #  colId = 17
-    #  tag = item
-    #  index = [5, 6, 12, 15, 30]
-    #  parts = [tag[i:j] for i,j in zip(index, index[1:] + [None])]
-    #  for id, items in enumerate(parts):
-    #     while ws.cell(row, col).value:
-    #       row+=1
-    #     ws[column[colId] + str(row)] = items
-    #     colId += 1
-    #     col += 1
-    else:
-      item = ""
-
-  print("Excel Updated")
-  
-  wb.save(file_path)
-
-def TxtExl_Hoja2():
-     #Abre un archivo txt, r = Read
-  file = open('src/hsbc.txt', 'r')
-  f = file.readlines()
-
-  # Abre un Excel YA creado
-  file_path = 'src/Prueba.xlsx'
-  wb = load_workbook(file_path)
-  ws = wb['Hoja2']  # or wb.active
-
-  #Guarda contenido del Txt en un List
-
-  newList = []
-  for line in f:
-    if line[-1] == '\n':
-      newList.append(line)
-    else:
-      newList.append(line)  
-
-  #Iteración en la newList e Inserción a Excel
-
-  for index, item in enumerate(newList):
-
-    # Variables
-    column = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    #Identifica el tipo de Tag
-    #"Switch"
-    #Caso Tag 61 // Arreglar el salto de Línea
-    if(item.startswith(":61:")):
+    elif(item.startswith(":61:")):
       row = 1
       col = 11
       colId = 10
@@ -201,3 +139,9 @@ def TxtExl_Hoja2():
   print("Excel Updated")
   
   wb.save(file_path)
+
+def Mes_Dia_Final(any_day):
+    # The day 28 exists in every month. 4 days later, it's always next month
+    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+    # subtracting the number of the current day brings us back one month
+    return next_month - datetime.timedelta(days=next_month.day)
--- Python_Tests/Python_Tests.py
+++ Python_Tests/Python_Tests.py
@@ -1,6 +1,7 @@
 #from Excel import TxtExl
-from TxtExcel import TxtExl
-from Tests import Test
+#from TxtExcel import TxtExl
+#from Tests import Test
+from CashManagement import TxtExl
 
 TxtExl()
 #Test()
